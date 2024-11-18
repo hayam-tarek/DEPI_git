561f77ec4cd41ffcc8011cf8d2c5faf5a7d1becd
hayam-flutter <hayam.tarek@sherkety.net>
2024-11-18 21:58:52 +0200

    Add printHelloGit method to HelloIti

--- HelloIti.java
+++ HelloIti.java
@@ -3,4 +3,8 @@ public class HelloIti {
     public void printHelloITI() {
         System.out.println("Hello, ITI!");
     }
+
+    public void printHelloGit() {
+        System.out.println("Hello, Git!");
+    }
 }
